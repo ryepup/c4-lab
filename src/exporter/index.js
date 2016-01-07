3a20d36b603b5dd851f4105849920ecbcdc339eb
Ryan Davis <ryan@mokeys.org>
2016-01-07 00:06:20 -0500

    Add "save as png"
    Hop through an `<img>` and a `<canvas>` to get the a data URI for a png
    format. Pretty silly, but works ok.
    
    fix #6

--- src/exporter/index.js
+++ src/exporter/index.js
@@ -1,13 +1,37 @@
 var Viz = require('viz.js');
 
-module.exports = function() {
-  var self = this;
+// @ngInject
+module.exports = function($window, $q) {
+  var self = this,
+      document = $window.document
+  ;
+
   self.toDOT = require('./dot');
   self.toJson = JSON.stringify;
   self.fromJson = JSON.parse;
   self.toSVG = toSVG;
+  self.toPNG = toPNG;
 
   function toSVG(graph) {
     return Viz(self.toDOT(graph), { format:"svg", engine:"dot" });
+  }
+
+  function toPNG(graph) {
+    var svg = toSVG(graph),
+        img = document.createElement("img")
+    ;
+    return $q(function(resolve, reject) {
+      img.onload = function() { resolve(getPNGDataURL(img)); };
+      img.setAttribute("src", "data:image/svg+xml," + encodeURIComponent(svg));
+    });
+  }
+
+  function getPNGDataURL(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    canvas.getContext("2d")
+      .drawImage(img, 0, 0);
+    return canvas.toDataURL("image/png");
   }
 };
--- src/saveAs.controller.js
+++ src/saveAs.controller.js
@@ -1,21 +1,28 @@
 
 // @ngInject
-module.exports = function(exporter, $window) {
+module.exports = function(exporter, $window, $q) {
   var vm = this;
 
   vm.formats = {
-        json: saveAs.bind(null, 'application/json', exporter.toJson),
-        dot: saveAs.bind(null, 'text/plain', exporter.toDOT),
-        svg: saveAs.bind(null, 'image/svg+xml', exporter.toSVG)
+    json: saveAs.bind(null, 'application/json', exporter.toJson),
+    dot: saveAs.bind(null, 'text/plain', exporter.toDOT),
+    svg: saveAs.bind(null, 'image/svg+xml', exporter.toSVG),
+    png: saveAs.bind(null, null, exporter.toPNG)
   };
 
   vm.saveAs = function(format) { vm.formats[format](format); };
 
   function saveAs(contentType, serializer, extension) {
-    var a = $window.document.createElement('a'),
-        content = serializer(vm.graph);
-    a.download = 'c4-graph.' + extension;
-    a.href = 'data:' + contentType + ',' + encodeURIComponent(content);
-    a.click();
+    $q.when(serializer(vm.graph))
+      .then(function(content) {
+        var a = $window.document.createElement('a');
+        a.download = 'c4-graph.' + extension;
+        if(contentType){
+          a.href = 'data:' + contentType + ',' + encodeURIComponent(content);
+        } else {
+          a.href = content;
+        }
+        a.click();
+      });
   }
 };
