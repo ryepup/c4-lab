32150768844ef4831d0795cde836428496136724
Ryan Davis <ryan@mokeys.org>
2016-01-07 23:24:45 -0500

    Expand the autosave with a timer
    `autoSave.saveEvery(graph, seconds)` kicks off a timer. Permits deleting
    a lot of manual plumbing to be sure we save after edits

--- src/autoSave.js
+++ src/autoSave.js
@@ -1,5 +1,7 @@
+var exporter = require('./exporter/json');
+
 // @ngInject
-module.exports = function($window, $timeout, exporter) {
+module.exports = function($window, $timeout, $interval) {
   var self = this,
       localStorage = $window.localStorage,
       key = 'C4-LAB-ACTIVE-DOC'
@@ -7,12 +9,23 @@ module.exports = function($window, $timeout, exporter) {
 
   self.load = load;
   self.save = save;
+  self.saveEvery = saveEvery;
 
   function load() { return exporter.fromJson(localStorage.getItem(key) || 'null'); }
   function save(graph) {
     return $timeout(function() {
       var json = exporter.toJson(graph);
       localStorage.setItem(key, json);
+      self.lastSaved = new Date();
     });
   }
-}
+
+  function saveEvery(graph, seconds) {
+    return $interval(saveIfModified.bind(null, graph), seconds);
+  }
+
+  function saveIfModified(graph) {
+    if(graph.lastModified > self.lastSaved){ save(graph); }
+  }
+
+};
--- src/shell.controller.js
+++ src/shell.controller.js
@@ -1,7 +1,9 @@
 // @ngInject
-module.exports = function(model, autoSave, exporter) {
+module.exports = function(autoSave, exporter) {
   var vm = this;
 
   vm.graph = autoSave.load()
     || exporter.fromJson(require('./exporter/c4-lab.json'));
+
+  autoSave.saveEvery(vm.graph, 5000);
 };
