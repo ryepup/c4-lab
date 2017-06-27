1bee5eba7e717d15e093c8f9ceeeeb1790378734
Ryan Davis <ryepup@gmail.com>
2017-06-26 22:35:57 -0400

    Remove dead code

--- src/header/about.component.js
+++ src/header/about.component.js
@@ -1,13 +1,8 @@
 import template from './about.html'
 
-class AboutComponent{
-
-}
-
 export const name = "c4LabAboutModal"
 export const options = {
     template: template,
-    controller: AboutComponent,
     bindings: {
         close: '&'
     }
